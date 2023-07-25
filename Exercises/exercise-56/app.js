24d4f6abc85af034f55000337991547602a55da9
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-07-25 19:10:50 +0100

    Update app.js - 56

--- Exercises/exercise-56/app.js
+++ Exercises/exercise-56/app.js
@@ -1,3 +1,18 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
 
 //YOU CODE GOES HERE:
+let allSpans = document.querySelectorAll('span');
+
+for (let i = 0; i < allSpans.length; i++)
+{
+    allSpans[i].style.color = colors[i];
+}
+
+// Other syntax:
+const span = document.querySelectorAll('span');
+
+let i = 0;
+for(let letter of span){
+    letter.style.color=`${colors[i]}`;
+    i++;
+}
