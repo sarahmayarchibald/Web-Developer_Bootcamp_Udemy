8492dda70be8e36416e618857516faedf57e75b3
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2023-07-25 18:28:36 +0100

    Create app.js

---
+++ Exercises/exercise-54/app.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const imgSrc = document.querySelector('img');
+
+imgSrc.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+imgSrc.setAttribute('alt', 'chicken');
